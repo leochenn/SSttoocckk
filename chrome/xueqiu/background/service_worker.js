e53bd2f22b15f3974da2735943e954219752ad73
ZengLei <zenglei@caohua.com>
2024-04-28 13:43:19 +0800

    chrome雪球更新优化

--- chrome/xueqiu/background/service_worker.js
+++ chrome/xueqiu/background/service_worker.js
@@ -1,36 +1,55 @@
+var switchState = 1
+var lastNotifyId = ''
+
 function log(tag, data) {
   console.log('leo:', tag + '->' + JSON.stringify(data))
 }
 
-var lastNotifyId = ''
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  if (message.action === 'title_changed' || message.action === 'content_changed') {
+  if (message.action === 'from_popup') {
+    log('popop', message)
+    if (message.data === 'get_state') {
+      // 向发送方回复消息
+      log('向popop发消息', switchState)
+      sendResponse({ message: switchState });
+    } else if (message.message === 'close') {
+      switchState = 0
+      log('关闭通知', switchState)
+    } else if (message.message === 'open') {
+      switchState = 1
+      log('开启通知', switchState)
+    }
+  } else if (message.action === 'title_changed' || message.action === 'content_changed') {
     log('执行指令', '')
     log(message.action, message.message)
     var title = message.action === 'title_changed' ? '标题更新' : '内容更新'
 
-    chrome.notifications.create({
-        type: "basic",
-        title: title,
-        message: message.message.msg,
-        iconUrl: "../icon/icon.png"
-      },
-      (notificationId) => {
-        lastNotifyId = notificationId
-        log('消息发送成功', notificationId)
-      }
-    );
-    
-    chrome.notifications.onClicked.addListener(function(notificationId) {
-        log('消息被点击', notificationId + ', ' + lastNotifyId)
-        if (JSON.stringify(lastNotifyId) == JSON.stringify(notificationId)) {
-          lastNotifyId = ''
-          // 当用户点击通知时，打开特定的网页
-          chrome.tabs.create({ url: "http://www.baidu.com" });
-        }        
-    });
+    if (switchState == 1) {
+      chrome.notifications.create({
+          type: "basic",
+          title: title,
+          message: message.message.msg,
+          iconUrl: "../icon/icon.png"
+        },
+        (notificationId) => {
+          lastNotifyId = notificationId
+          log('消息发送成功', notificationId)
+        }
+      );
+      
+      chrome.notifications.onClicked.addListener(function(notificationId) {
+          log('消息被点击', notificationId + ', ' + lastNotifyId)
+          if (JSON.stringify(lastNotifyId) == JSON.stringify(notificationId)) {
+            lastNotifyId = ''
+            // 当用户点击通知时，打开特定的网页
+            chrome.tabs.create({ url: "http://www.baidu.com" });
+          }        
+      });
+    } else {
+      log('通知已关闭', '!!!')
+    }
   } else {
-    log('收到日志', message)
+    log('收到日志', message)    
   }
 });
 
--- chrome/xueqiu/popup/index.js
+++ chrome/xueqiu/popup/index.js
@@ -1,10 +1,34 @@
 const plugin_search_but = document.getElementById('plugin_search_but')
-const plugin_search_inp = document.getElementById('plugin_search_inp')
+const plugin_search_state = document.getElementById('plugin_search_state')
+
 plugin_search_but.onclick = function () {
-    // alert('plugin_search_inp的值为：' + plugin_search_inp.value.trim())
-    console.log('chrome', chrome)
-    chrome.runtime.sendMessage({
-    action: 'fromPopup',
-    message: 'Hello from Popup!'
-  });
+    if (state == 0) {
+        state = 1;
+        chrome.runtime.sendMessage({
+            action: 'from_popup',
+            message: 'open'
+        });
+        plugin_search_state.value = '已开启'
+    } else {
+        state = 0;
+        chrome.runtime.sendMessage({
+            action: 'from_popup',
+            message: 'close'
+        });
+        plugin_search_state.value = '已关闭'
+    }
 }
+
+var state = 0;
+
+chrome.runtime.sendMessage({ action: "from_popup", data: "get_state" }, function(response) {
+    var aState = response.message;
+    if (aState == 0) {
+        plugin_search_state.value = '已关闭'
+        state = 0;
+    } else {
+        plugin_search_state.value = '已开启'
+        state = 1;
+    }    
+});
+
