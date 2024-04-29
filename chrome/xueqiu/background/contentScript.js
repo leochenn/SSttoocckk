ec50f380ae93a95aa56fbf7fa40eb1381404df47
ZengLei <zenglei@caohua.com>
2024-04-29 09:57:49 +0800

    优化

--- chrome/xueqiu/background/contentScript.js
+++ chrome/xueqiu/background/contentScript.js
@@ -1,3 +1,10 @@
 // 雪球页面滑动到顶部
 console.log('leo:雪球页面滑动到顶部')
 window.scrollTo(0, 0);
+const elementToClick = document.querySelector('.timeline__tab__tags a.active')
+if (elementToClick && elementToClick.textContent.trim() === '全部') {
+	console.log('leo:点击全部，进行刷新');
+	elementToClick.click();
+} else {
+	console.log('leo:Element not found');
+}
