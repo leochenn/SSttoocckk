9f54ebb90bd3d18b3c037ee54543f04b1d064e61
ZengLei <zenglei@caohua.com>
2024-05-10 15:01:52 +0800

    python-操作pc微信发送消息

--- python/AutoSystem/wechat.py
+++ python/AutoSystem/wechat.py
@@ -13,9 +13,11 @@ import win32gui
 import log
 import chlog
 
-if __name__ == '__main__':
-    log.setTag("xxx")
-    chlog.setTag("xxx")
+from pywinauto import Application
+from pywinauto.keyboard import send_keys
+
+# 方法一：通过pyautogui
+def send_msg1():
     hwnd = win32gui.FindWindow('WeChatMainWndForPC', None)
     win32gui.ShowWindow(hwnd, win32con.SW_SHOWNOACTIVATE)
     win32gui.SetActiveWindow(hwnd)
@@ -51,3 +53,44 @@ if __name__ == '__main__':
     # 点击发送按钮
     win32api.SetCursorPos([right - 80, bottom - 30])
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+
+def send_msg2():
+    app = Application(backend="uia").connect(title_re="微信")
+    chlog.e('connect', app)
+    dlg = app.window(title='微信')
+    dlg.set_focus()
+    chlog.e('app.window', dlg)
+
+    # dlg.print_control_identifiers()
+
+    time.sleep(0.2)
+
+    a = '1马甲群已置顶'
+    b = '文件传输助手'
+    fileSend = dlg.child_window(title=a, control_type='ListItem')
+    fileSend.print_control_identifiers()
+
+    fileSend.click_input()
+    chlog.e('fileSend.click_input')
+    time.sleep(0.2)
+
+    # dlg.child_window(title='文件传输助手', control_type='Edit').click_input()
+    # chlog.e('fileSend.click_input Edit')
+    # time.sleep(2)
+
+    send_keys('321我是谁')
+    chlog.e('SendKeys')
+    time.sleep(0.1)
+
+    sendBtn = dlg.child_window(title='发送(S)', control_type='Button')
+    chlog.e('发送')
+    time.sleep(0.2)
+
+    sendBtn.click_input()
+
+
+# 通过俩种不同的方式来实现pc微信发送消息，第二种更为灵活，可以获取到控件树结构
+if __name__ == '__main__':
+    log.setTag("xxx")
+    chlog.setTag("xxx")
+    send_msg2()
